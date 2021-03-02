37e3c81bd0d973322c1c6ecfe2f253f76b48e121
Yue Lou <67313751+teggs@users.noreply.github.com>
2021-03-02 11:58:41 +0800

    Update Compile_asm_file.sh

--- Compile_asm_file.sh
+++ Compile_asm_file.sh
@@ -3,4 +3,4 @@ nasm -f elf64 file.asm
 #elf64 create an object executable in x86_64 system. elf32 works in x86 system.
 
 ld -s -o file file.o
-#ld, the GUN linker, creates a executable file for WSL.
+#ld, the GUN linker, creates an executable file for WSL.
