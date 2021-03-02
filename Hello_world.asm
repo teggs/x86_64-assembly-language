525ecc25c60b2a0fe37da635718e6a09c545286a
Yue Lou <67313751+teggs@users.noreply.github.com>
2021-03-02 11:50:07 +0800

    Update Hello_world.asm

--- Hello_world.asm
+++ Hello_world.asm
@@ -15,20 +15,20 @@ _start:
   mov rdi, 0
   syscall
   
-  COMMENT @
-  Appendix: System Call List
-  syscall    ID(rax) ARG1(rdi)       ARG2(rsi)  ARG3(rdx)  ARG4(r10)
-  sys_read     0     file descriptor buffer     count
-  sys_write    1     file descriptor buffer     count
-  sys_open     2     file name       flag       mode
-  sys_close    3     file descriptor
-  sys_exit     60    error code
+
+  ;Appendix: System Call List
+  ;syscall    ID(rax) ARG1(rdi)       ARG2(rsi)  ARG3(rdx)  ARG4(r10)
+  ;sys_read     0     file descriptor buffer     count
+  ;sys_write    1     file descriptor buffer     count
+  ;sys_open     2     file name       flag       mode
+  ;sys_close    3     file descriptor
+  ;sys_exit     60    error code
   
-  file descriptor: 0(standard input), 1(standard output), 2(standard error)
-  buffer: location of the string
-  count: length of the string
+  ;file descriptor: 0(standard input), 1(standard output), 2(standard error)
+  ;buffer: location of the string
+  ;count: length of the string
   
-  Therefore, if one wants to output a string. He needs to invoke sys_write, that is,
-  rax = 1, rdi = 1, rsi = text, rdx = len
-  This is the same reason for sys_exit.
-  @
+  ;Therefore, if one wants to output a string. He needs to invoke sys_write, that is,
+  ;rax = 1, rdi = 1, rsi = text, rdx = len
+  ;This is the same reason for sys_exit.
+ 
