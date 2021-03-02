bc74679bf6677068809ecb97026aa2a199fd72dc
Yue Lou <67313751+teggs@users.noreply.github.com>
2021-03-02 13:45:30 +0800

    Update Receive_user's_input.asm

--- Receive_user's_input.asm
+++ Receive_user's_input.asm
@@ -1,11 +1,11 @@
-section .data
+section .data ;see Appendix, Three Sections of x84_64 Assembly File
   text1 db "Input your name: "
-  len1 equ $- text1
+  len1 equ $- text1 ;len1 should be calculated first then text2 should be defined. Otherwise text2 will follow the text1 and the len1 will equal to len2.
   text2 db "Hello, "
   len2 equ $- text2
 
 section .bss      
-  name resb 16
+  name resb 16  ;reserve 16 bytes for user's input.
 
 section .text
   global _start
@@ -16,38 +16,49 @@ _start:
   call _printText2
   call _printName
   
-  mov rax,60
-  mov rdi,0
+  mov rax, 60
+  mov rdi, 0
   syscall
   
 _printText1:                            
-  mov rax,1
-  mov rdi,1
-  mov rsi,text1
-  mov rdx,len1
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, text1
+  mov rdx, len1
   syscall
   ret
   
-_getName:
-  mov rax,0
-  mov rdi,0
-  mov rsi,name
-  mov rdx,16
+_getName: ;this part invoke the sys_write function. So its parameters are different from other 3 parts.
+  mov rax, 0
+  mov rdi, 0
+  mov rsi, name
+  mov rdx, 16
   syscall
   ret
   
 _printText2:
-  mov rax,1
-  mov rdi,1
-  mov rsi,text2
-  mov rdx,len2
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, text2
+  mov rdx, len2
   syscall
   ret
   
 _printName:
-  mov rax,1
-  mov rdi,1
-  mov rsi,name
-  mov rdx,16
+  mov rax, 1
+  mov rdi, 1
+  mov rsi, name
+  mov rdx, 16
   syscall
   ret
+
+
+;Appendix: Three Sections of x84_64 Assembly File
+; .data section
+;data section is where all data are defined before compilation.
+
+; .bss section
+;bss section is where data are allocated for future use.
+
+; .text section
+;text section is where the actual code goes.
